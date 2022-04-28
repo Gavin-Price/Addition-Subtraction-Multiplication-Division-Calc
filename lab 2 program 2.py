f2548a96e6fa7d1f2d6d7d33f413213fed694a64
Gavin-Price <95958548+Gavin-Price@users.noreply.github.com>
2022-04-28 18:50:28 -0500

    Add files via upload

---
+++ lab 2 program 2.py
@@ -0,0 +1,20 @@
+interger1 = int(input('Enter number:'))
+
+interger2 = int(input('Enter number:'))
+
+sum = interger1 + interger2
+
+print('Sum:' , sum)
+
+product = interger1 * interger2
+
+print('Product:' , product)
+
+difference = interger1 - interger2
+
+print('Difference:' , difference) 
+
+quotient = interger1 / interger2 
+
+print('Quotient:' , quotient)
+
